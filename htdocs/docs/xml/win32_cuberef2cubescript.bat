d0e4f4b5081f691ded784082066a1b7cc7484596
RandumKiwi <RandumKiwi@users.noreply.github.com>
2012-10-06 20:17:20 +0000

    minor fix
    git-svn-id: svn://svn.code.sf.net/p/actiongame/code/ac_website@6905 e016b1c5-ed68-44aa-a2f7-31cac88cffde

--- htdocs/docs/xml/win32_cuberef2cubescript.bat
+++ htdocs/docs/xml/win32_cuberef2cubescript.bat
@@ -3,7 +3,7 @@ rem Automatically allows MS Windows to transform ../reference.xml into ../../con
 rem This requires MSXSL, available at: http://www.microsoft.com/en-us/download/details.aspx?id=21714
 @echo on
 
-msxsl.exe -o ..\..\config\docs.cfg -v ..\reference.html ..\xml\cuberef2cubescript.xslt
+msxsl.exe -o ..\..\config\docs.cfg -v ..\reference.xml cuberef2cubescript.xslt
 
 echo THIS BATCH
 echo IS FINISHED!
