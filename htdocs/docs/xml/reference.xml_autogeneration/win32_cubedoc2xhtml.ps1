6763806e0d8db586736fb93b6b57e1cdae9eb2ca
RandumKiwi <RandumKiwi@users.noreply.github.com>
2012-10-06 19:32:47 +0000

    This was used to convert .xml files to .xhtml, so is now redundant. Deleted.
    git-svn-id: svn://svn.code.sf.net/p/actiongame/code/ac_website@6900 e016b1c5-ed68-44aa-a2f7-31cac88cffde

--- htdocs/docs/xml/reference.xml_autogeneration/win32_cubedoc2xhtml.ps1
+++
@@ -1,8 +0,0 @@
-$files = dir ..\* -include *.html
-
-foreach($f in $files)
-{
-    $in = $f.FullName
-    $out = [system.io.path]::ChangeExtension($f.FullName, "html")
-    msxsl.exe -o "$out" -v "$in" ..\xml\cubedoc2xhtml.xslt
-}
